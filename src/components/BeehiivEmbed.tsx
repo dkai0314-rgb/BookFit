0d4d02c515c6072504749fcd8efeab2bd589323f
dkai0314-rgb <dkai0314@gmail.com>
2026-02-22 01:43:58 +0900

    update-beehiiv-embed-code

--- src/components/BeehiivEmbed.tsx
+++ src/components/BeehiivEmbed.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export default function BeehiivEmbed() {
-    // This is a placeholder for the Beehiiv embed code.
-    // User needs to replace this with their actual embed code from Beehiiv.
-
-    // Example structure provided by Beehiiv usually looks like:
-    // <iframe ...></iframe>
-    // <script ...></script>
-
     useEffect(() => {
         const script = document.createElement("script");
         script.src = "https://subscribe-forms.beehiiv.com/embed.js";
@@ -22,17 +15,21 @@ export default function BeehiivEmbed() {
     }, []);
 
     return (
-        <div className="w-full max-w-[320px] sm:max-w-md mx-auto flex justify-center px-2 sm:px-0">
+        <div className="w-full flex justify-center">
             <iframe
                 src="https://subscribe-forms.beehiiv.com/fa2ef120-ce5b-4bce-9201-5f5b40f4d753"
+                className="beehiiv-embed"
                 data-test-id="beehiiv-embed"
                 frameBorder="0"
                 scrolling="no"
-                className="w-full h-[300px] sm:h-[350px]"
                 style={{
+                    width: "397px",
+                    height: "230px",
                     margin: 0,
                     borderRadius: "0px",
                     backgroundColor: "transparent",
+                    boxShadow: "0 0 #0000",
+                    maxWidth: "100%",
                 }}
             />
         </div>
